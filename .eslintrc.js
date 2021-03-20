ff01d66557859580aa9412696dac152e08c0c6f5
daniel <danielhdwijaya@gmail.com>
2021-03-21 01:11:50 +0700

    update lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
-  "rules": {
-    "prettier/prettier": ["error", {
-      "endOfLine":"auto"
-    }]
-  }
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
 };
-  

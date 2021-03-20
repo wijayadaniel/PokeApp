a124961345143d0367037f022acc742f2914249a
daniel <danielhdwijaya@gmail.com>
2021-03-21 02:31:56 +0700

    update main pages

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import React, {useState} from 'react';
+import AppLoading from 'expo-app-loading';
+import {Asset} from 'expo-asset';
+import * as Font from 'expo-font';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import {NavigationContainer} from '@react-navigation/native';
+import {persistor, store} from './src/redux/store';
+import MainStack from './src/navigation/MainStack';
 
 export default function App() {
+  const [assetsLoaded, setAssetsLoaded] = useState(false);
+
+  const cacheImages = (images) => {
+    return images.map((image) => {
+      return Asset.fromModule(image).downloadAsync();
+    });
+  };
+
+  const cacheFonts = (fonts) => {
+    return fonts.map((font) => Font.loadAsync(font));
+  };
+
+  const loadAssetsAsync = async () => {
+    const imageAssets = cacheImages([require('./assets/img/placeholder.png')]);
+    const fontAssets = cacheFonts([
+      {VT323: require('./assets/fonts/VT323-Regular.ttf')},
+    ]);
+    await Promise.all([...imageAssets, ...fontAssets]);
+  };
+
+  if (!assetsLoaded) {
+    return (
+      <AppLoading
+        startAsync={loadAssetsAsync}
+        onFinish={() => setAssetsLoaded(true)}
+        onError={console.warn}
+      />
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <StatusBar style="dark" />
+        <NavigationContainer>
+          <MainStack />
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

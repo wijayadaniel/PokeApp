7d8e042de941647062a09dce7c3e3696a91b7f82
daniel <danielhdwijaya@gmail.com>
2021-03-22 01:57:25 +0700

    add new compare navigation

--- src/navigation/MainStack.js
+++ src/navigation/MainStack.js
@@ -2,14 +2,29 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import Main from '../pages/Main';
 import Details from '../pages/Details';
+import CompareButton from '../components/atoms/button/compare';
+import Compare from '../pages/Compare';
 
 const MainStack = () => {
   const Stack = createStackNavigator();
 
   return (
     <Stack.Navigator initialRouteName="Main">
-      <Stack.Screen name="Poke Apps" component={Main} />
+      <Stack.Screen
+        name="Poke Apps"
+        component={Main}
+        options={{
+          headerRight: () => <CompareButton action={'Navigation'} />,
+        }}
+      />
       <Stack.Screen name="Details" component={Details} />
+      <Stack.Screen
+        name="Compare"
+        component={Compare}
+        options={{
+          headerTitle: 'Compare Pokemon',
+        }}
+      />
     </Stack.Navigator>
   );
 };

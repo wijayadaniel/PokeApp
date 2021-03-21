d164ec70c535a779a4aab8da127769d54dfb47df
daniel <danielhdwijaya@gmail.com>
2021-03-22 03:38:04 +0700

    pages details

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,10 +1,42 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-const Details = () => {
+import {SafeAreaView, ScrollView, View, Text, StyleSheet} from 'react-native';
+import {scale} from 'react-native-size-matters';
+import DetailsDamageList from '../components/atoms/pokemon-detail/demageList';
+import DetailImages from '../components/atoms/pokemon-detail/detailImages';
+import DetailsInfo from '../components/atoms/pokemon-detail/info';
+import PokemonType from '../components/atoms/pokemon-type';
+import {InfoParams, StatusParams} from '../helpers/common';
+
+const Details = ({route}) => {
+  const {pokemonDetail} = route.params;
+
   return (
-    <View>
-      <Text>page detail</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <ScrollView>
+          <DetailImages sprites={pokemonDetail.sprites} />
+          <View style={styles.typeContainer}>
+            <PokemonType types={pokemonDetail.types} details_flag={1} />
+          </View>
+          <DetailsInfo header={'Info: '} params={InfoParams(pokemonDetail)} />
+          <DetailsInfo
+            header={'Status: '}
+            params={StatusParams(pokemonDetail)}
+          />
+          <DetailsDamageList typeList={pokemonDetail.types} />
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   );
 };
+const styles = StyleSheet.create({
+  container: {flex: 1, backgroundColor: 'white'},
+  typeContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginVertical: scale(20),
+  },
+});
+
 export default Details;

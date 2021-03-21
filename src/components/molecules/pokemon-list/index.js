91b973e53f633f934f7c9b5ad68b6ac40ca14aba
daniel <danielhdwijaya@gmail.com>
2021-03-22 01:56:44 +0700

    update handle action button

--- src/components/molecules/pokemon-list/index.js
+++ src/components/molecules/pokemon-list/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import {ScrollView} from 'react-native';
-import LoadMoreButton from '../../atoms/load-more-button';
+import ActionButton from '../../atoms/button/action';
 import PokemonCard from '../pokemon-card';
 
 const PokemonList = ({pokemonList, onLoadMore}) => {
-  const renderCard = (item) => {
-    return <PokemonCard name={item.name} url={item.url} />;
+  const renderCard = (item, i) => {
+    return <PokemonCard key={i} name={item.name} url={item.url} />;
   };
 
   return (
     <ScrollView style={{flex: 1, width: '100%'}}>
       {pokemonList.length > 0 ? pokemonList.map(renderCard) : null}
-      <LoadMoreButton onLoadMore={onLoadMore} />
+      <ActionButton title={'Load More'} onPress={onLoadMore} />
     </ScrollView>
   );
 };

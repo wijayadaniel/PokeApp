4faa02ed1359dd244fcae294ffd56afe9c5026be
daniel <danielhdwijaya@gmail.com>
2021-03-21 18:14:55 +0700

    code detail pages

---
+++ src/pages/Details.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+const Details = () => {
+  return (
+    <View>
+      <Text>page detail</Text>
+    </View>
+  );
+};
+export default Details;
